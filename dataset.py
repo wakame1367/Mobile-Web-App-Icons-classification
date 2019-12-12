a7730f8054e269847e2159e128f9753a737b4124
kot.yuhara <kot.yuhara@trifort.jp>
2019-12-12 16:35:52 +0900

    add argument

--- dataset.py
+++ dataset.py
@@ -1,12 +1,8 @@
-from pathlib import Path
-
 import pandas as pd
 
-dataset_path = Path("../resources/common-mobile-web-app-icons/")
 
-
-def create_df():
-    image_paths = [path for path in dataset_path.glob("*/*.jpg")]
+def create_df(root_path):
+    image_paths = [path for path in root_path.glob("*/*.jpg")]
     x_col_name = "image_path"
     y_col_name = "class"
     df = pd.DataFrame({x_col_name: image_paths})
