3fb11917ae92c4a254f074f5b332e74c68c28781
kot.yuhara <kot.yuhara@trifort.jp>
2019-12-12 16:37:50 +0900

    add create csv

---
+++ run.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from dataset import create_df
+
+dataset_path = Path("../resources")
+image_root_path = dataset_path / "common-mobile-web-app-icons"
+
+
+def main():
+    df_path = dataset_path / "data.csv"
+    df = create_df(root_path=image_root_path)
+    df.to_csv(df_path, index=False)
+
+
+if __name__ == '__main__':
+    main()
