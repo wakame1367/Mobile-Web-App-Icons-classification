65d7515167b993a43c6ff3c098714d5f86deeb9a
kot.yuhara <kot.yuhara@trifort.jp>
2019-12-17 16:28:16 +0900

    add sequencer

--- dataloader.py
+++ dataloader.py
@@ -1,18 +1,25 @@
 import warnings
+from pathlib import Path
 
 import cv2
 import numpy as np
+import pandas as pd
+from PIL import Image
 from albumentations import (
     Compose, HueSaturationValue, RandomBrightness, RandomContrast, RandomGamma,
     ToFloat, Cutout, HorizontalFlip, PadIfNeeded, RandomCrop, RandomScale,
     Resize, Rotate
 )
 from albumentations import ImageOnlyTransform
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.python.keras.utils import to_categorical
 from tensorflow.python.keras.utils.data_utils import Sequence
 
 warnings.filterwarnings('ignore')
 
 Height, Width = 224, 224
+dataset_path = Path("./resources")
+image_root_path = dataset_path / "common-mobile-web-app-icons"
 
 
 class GrayToRGB(ImageOnlyTransform):
@@ -67,26 +74,48 @@ AUGMENTATIONS_VALID = Compose([
 ])
 
 
-class MNISTSequence(Sequence):
-    def __init__(self, x_set, y_set, batch_size, augmentations):
-        self.x, self.y = x_set, y_set
+class MobileAppImageSequence(Sequence):
+    def __init__(self, image_path, y, batch_size, target_size, augmentations):
+        self.image_paths = image_path
+        self.y = y
         self.batch_size = batch_size
+        self.target_size = target_size
         self.augment = augmentations
 
     def __len__(self):
-        return int(np.ceil(len(self.x) / float(self.batch_size)))
+        return int(np.ceil(len(self.image_paths) / float(self.batch_size)))
+
+    def get_image(self, batch_paths):
+        for img_path in batch_paths:
+            yield np.array(Image.open(img_path))
 
     def __getitem__(self, idx):
-        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        start_idx = idx * self.batch_size
+        end_idx = (idx + 1) * self.batch_size
+        paths = self.image_paths[start_idx:end_idx]
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
         return np.stack([
-            self.augment(image=x)["image"] for x in batch_x
-        ], axis=0), np.array(batch_y)
+            self.augment(image=x)["image"] for x in self.get_image(paths)],
+            axis=0), np.array(batch_y)
 
 
 def main():
-    pass
+    df = pd.read_csv(dataset_path / "data.csv")
+    x_col = "image_path"
+    y_col = "class"
+    batch_size = 10
+    num_classes = df[y_col].nunique()
+    target_size = (Height, Width)
+    label_encoder = LabelEncoder()
+    label_encoder.fit(df[y_col])
+    label = to_categorical(label_encoder.transform(df[y_col]),
+                           num_classes=num_classes)
+    gen = MobileAppImageSequence(image_path=df[x_col], y=label,
+                                 target_size=target_size, batch_size=batch_size,
+                                 augmentations=AUGMENTATIONS_KMNIST)
+    imgs, label = gen.__getitem__(0)
+    print(imgs.shape, label.shape)
 
 
 if __name__ == '__main__':
